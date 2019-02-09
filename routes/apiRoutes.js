9fea5c612972a7c9ef183509b32cf3026a4477e7
Chieftjz <taylor.zlomke@gmail.com>
2019-02-08 23:31:35 -0700

    formatted tabs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,24 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Get all plants
-  app.get("/api/plants", function(req, res) {
-    db.Plant.findAll({}).then(function(dbPlants) {
-      res.json(dbPlants);
-    });
-  });
+module.exports = function (app) {
+	// Get all plants
+	app.get("/api/plants", function (req, res) {
+		db.Plant.findAll({}).then(function (dbPlants) {
+			res.json(dbPlants);
+		});
+	});
 
-  // Add a plant to database
-  app.post("/api/plants", function(req, res) {
-    db.Plant.create(req.body).then(function(dbPlants) {
-      res.json(dbPlants);
-    });
-  });
+	// Add a plant to database
+	app.post("/api/plants", function (req, res) {
+		db.Plant.create(req.body).then(function (dbPlants) {
+			res.json(dbPlants);
+		});
+	});
 
-  // Delete an example by id
-  // app.delete("/api/examples/:id", function(req, res) {
-  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.json(dbExample);
-  //   });
-  // });
+	// Delete an example by id
+	// app.delete("/api/examples/:id", function(req, res) {
+	//   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+	//     res.json(dbExample);
+	//   });
+	// });
 };
