34a4cb9a9d988aea303c5c3a5c06ff4469b57af9
Chieftjz <taylor.zlomke@gmail.com>
2019-02-08 23:30:43 -0700

    created api routes for plant database

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,24 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Get all plants
+  app.get("/api/plants", function(req, res) {
+    db.Plant.findAll({}).then(function(dbPlants) {
+      res.json(dbPlants);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // Add a plant to database
+  app.post("/api/plants", function(req, res) {
+    db.Plant.create(req.body).then(function(dbPlants) {
+      res.json(dbPlants);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  // app.delete("/api/examples/:id", function(req, res) {
+  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
 };
