92a27c0a8cf4d9b28bc3e9c6833f03c8ef4b8e43
Chieftjz <taylor.zlomke@gmail.com>
2019-02-12 20:40:12 -0700

    POST on click for calendar events set up

--- public/js/eventcalendar.js
+++ public/js/eventcalendar.js
@@ -1,8 +1,7 @@
 $(function () {
-	$.ajax({
-		type: "GET",
-		url: "/api/events",
-	}).done(function(doc) {
+
+	// GET Events
+	$.get("/api/events").done(function(doc) {
 		var event = Array();
 		$.each(doc, function(i, entry) {
 			event.push({
@@ -18,4 +17,24 @@ $(function () {
 			displayEventTime: false
 		});
 	});
+
+	// POST New Events
+	$(".addEvent").on("click", function() {
+		var eventName = $(this).data("name");
+		var eventType = $(this).data("type");
+		$.post("/api/events", {
+			event_name: eventName,
+			event_type: eventType,
+			time_start: "00:00",
+			time_end: "23:59",
+			daysOfWeek: [1, 4],
+			date_start: moment().format("YYYY MM DD"),
+			date_end: moment().add(100, "years").format("YYYY MM DD"),
+			UserId: 1
+		},
+		function(data, status){
+			window.location.href = "/mygarden/:username";
+		});
+	});
 });
+
